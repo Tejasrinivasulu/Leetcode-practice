19d03b2747b20fc4662572bbcb0f513261a41f9c
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2025-10-18 22:04:31 +0530

    Time: 1 ms (99.66%), Space: 46.4 MB (26.66%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[lastNonZeroFoundAt++] = num;
+            }
+        }
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
