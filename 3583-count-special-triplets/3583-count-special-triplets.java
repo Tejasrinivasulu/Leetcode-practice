3df6675a5f6306a0910e7cdeaa0af89eb881b239
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2025-12-09 07:29:12 +0530

    Time: 62 ms (89.92%), Space: 276.5 MB (5.43%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.java
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -2,30 +2,20 @@ class Solution {
     public int specialTriplets(int[] nums) {
         int n = nums.length;
         long MOD = 1_000_000_007;
-        
-        // Use arrays for O(1) constant time access since max value is 10^5
-        // We need size 200,001 because targetVal (nums[j]*2) can reach 200,000
         int[] leftFreq = new int[200001];
         int[] rightFreq = new int[200001];
-        
-        // 1. Pre-fill the right frequency array (suffix counts)
         for (int num : nums) {
             rightFreq[num]++;
         }
         
         long totalTriplets = 0;
         
-        // 2. Iterate through as index 'j' being the middle element
         for (int j = 0; j < n; j++) {
             int currentVal = nums[j];
             int targetVal = currentVal * 2;
             
-            // Remove current nums[j] from right counts so it's not counted as 'k'
             rightFreq[currentVal]--;
             
-            // If the target (2 * nums[j]) exists on both sides, add to result
-            // Check targetVal < 200001 is technically redundant due to nums[i] constraints
-            // but keeps the code safe from index out of bounds.
             if (targetVal <= 200000) {
                 long leftCount = leftFreq[targetVal];
                 long rightCount = rightFreq[targetVal];
@@ -35,7 +25,6 @@ class Solution {
                 }
             }
             
-            // Add current nums[j] to left counts for the next iteration
             leftFreq[currentVal]++;
         }
         
