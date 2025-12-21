a93f9fd731160a468da4b6154687033300bbd524
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2025-12-21 08:14:52 +0530

    Time: 0 ms (100%), Space: 43.8 MB (27.4%) - LeetHub

---
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        boolean[] sorted = new boolean[n - 1];
+        int delete = 0;
+
+        for (int col = 0; col < m; col++) {
+            boolean bad = false;
+
+            for (int row = 0; row < n - 1; row++) {
+                if (!sorted[row] &&
+                    strs[row].charAt(col) > strs[row + 1].charAt(col)) {
+                    bad = true;
+                    break;
+                }
+            }
+
+            if (bad) {
+                delete++;
+            } else {
+                for (int row = 0; row < n - 1; row++) {
+                    if (!sorted[row] &&
+                        strs[row].charAt(col) < strs[row + 1].charAt(col)) {
+                        sorted[row] = true;
+                    }
+                }
+            }
+        }
+        return delete;
+    }
+}
