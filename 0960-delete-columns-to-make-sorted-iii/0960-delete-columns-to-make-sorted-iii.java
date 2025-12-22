5b83d5226dd7b0f4ace47a7bd58df91c49993a83
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2025-12-22 09:36:31 +0530

    Time: 8 ms (58.97%), Space: 44.6 MB (11.54%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+
+        int[] dp = new int[m];
+        int maxKeep = 1;
+
+        for (int j = 0; j < m; j++) {
+            dp[j] = 1;
+            for (int i = 0; i < j; i++) {
+                boolean ok = true;
+                for (int r = 0; r < n; r++) {
+                    if (strs[r].charAt(i) > strs[r].charAt(j)) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+            maxKeep = Math.max(maxKeep, dp[j]);
+        }
+
+        return m - maxKeep;
+    }
+}
