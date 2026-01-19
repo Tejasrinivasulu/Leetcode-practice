84c48f976df6cad5b27fd0fffa12d546b0a55e9b
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2026-01-19 22:31:48 +0530

    Time: 7 ms (82.74%), Space: 57.9 MB (27.92%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxSideLength(int[][] mat, int threshold) {
+        int m = mat.length, n = mat[0].length;
+        int[][] pre = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                pre[i][j] = mat[i - 1][j - 1]
+                          + pre[i - 1][j]
+                          + pre[i][j - 1]
+                          - pre[i - 1][j - 1];
+            }
+        }
+
+        int left = 0, right = Math.min(m, n);
+
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (existsSquare(pre, m, n, mid, threshold)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean existsSquare(int[][] pre, int m, int n, int len, int threshold) {
+        for (int i = len; i <= m; i++) {
+            for (int j = len; j <= n; j++) {
+                int sum = pre[i][j]
+                        - pre[i - len][j]
+                        - pre[i][j - len]
+                        + pre[i - len][j - len];
+                if (sum <= threshold) return true;
+            }
+        }
+        return false;
+    }
+}
