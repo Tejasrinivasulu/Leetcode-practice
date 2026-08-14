964a9335c8c6431f47be0e3b04fda96b4fc1cea2
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2026-08-14 09:58:58 +0530

    Time: 0 ms (100%), Space: 44.8 MB (82.86%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        boolean found=false;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                found=true;
+                break;
+            }
+        }
+        if(found){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
