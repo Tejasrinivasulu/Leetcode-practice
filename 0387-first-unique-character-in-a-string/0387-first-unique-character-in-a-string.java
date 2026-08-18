7a76d7fd78a04f2e02b65899c3bba0274412eae7
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2026-08-18 10:13:31 +0530

    Time: 35 ms (13.14%), Space: 47 MB (39.97%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            boolean repeated = false;
+            for (int j = 0; j < s.length(); j++) {
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    repeated = true;
+                    break;
+                }
+            }
+            if (!repeated) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
