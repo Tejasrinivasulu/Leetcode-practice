644f3be373755f6e43620edd92db489c9863d9fe
Boya Teja Srinivasulu <163565201+Tejasrinivasulu@users.noreply.github.com>
2026-09-02 22:51:10 +0530

    Time: 2 ms (14.62%), Space: 42.2 MB (79.34%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n, fast = n;
-        do {
-            slow = sqSum(slow);
-            fast = sqSum(sqSum(fast));
-        } while (slow != fast);
-        return slow == 1;
-    }
-    private int sqSum(int x) {
-        int s = 0;
-        while (x > 0) {
-            int d = x % 10;
-            s += d * d;
-            x /= 10;
+         HashSet<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+
+            if (set.contains(n)) {
+                return false;
+            }
+
+            set.add(n);
+
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+
+            n = sum;
         }
-        return s;
+
+        return true;
     }
 }
